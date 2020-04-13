0543d8ee2875d31beda418d338b5894f541a374f
Stelios <stelios.phan@gmail.com>
2020-04-13 01:25:31 -0400

    Update databaseLogin.php

--- databaseLogin.php
+++ databaseLogin.php
@@ -1,6 +1,6 @@
 <?php
 	$servername = "c584md9egjnm02sk.cbetxkdyhwsb.us-east-1.rds.amazonaws.com"; 
-	$username = "	fzylhc5kajoe2fgm"; 
+	$username = "fzylhc5kajoe2fgm"; 
 	$password = "x2z8chywgkojoam3"; 
 	$dbName = "qgnighknygmz9zba"; 
 
