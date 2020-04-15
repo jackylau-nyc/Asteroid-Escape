3882cb3a48726c6f4003a4e9b19cb63467d3969e
Jacky Lau <jackylau.email@gmail.com>
2020-04-15 17:40:56 -0400

    fixed database

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php	
 	$username = '';
 	$password = '';
-	if(isset($_COOKIE['username']) && isset($_COOKIE['password'])) {
+	if(isset($_COOKIE['username']) && isset($_COOKIE['pass'])) {
 		$username = $_COOKIE['username'];
-		$password = $_COOKIE['password'];
+		$password = $_COOKIE['pass'];
 	}
 ?>
 <!DOCTYPE html>
@@ -22,7 +22,7 @@
 			  <div id = "logInBtns">
 				<!--<button style= "width:100%" id = "loginbtn" onclick="pauseGame(); document.getElementById('login').style.display='block'; listening = false;" style="width:auto;">Login</button>-->
 				<input type="text" placeholder="Enter Username" value = "<?php echo $username ?>" name="uname" id="loginUsername" class ="loginField" onFocus="if (deadTimer == -1) pauseGame(); document.getElementById('logInResponse').innerHTML='';" autofocus>
-				<input type="password" placeholder="Enter Password" value = "<?php echo $password ?>" name="psw" id="loginPassword" class ="loginField" onFocus="if (deadTimer == -1) pauseGame(); document.getElementById('logInResponse').innerHTML='';">
+				<input type="password" placeholder="Enter Password" value = "<?php echo $pass ?>" name="psw" id="loginPassword" class ="loginField" onFocus="if (deadTimer == -1) pauseGame(); document.getElementById('logInResponse').innerHTML='';">
 				<span class="error"><div id="logInResponse"></div></span>
 				<button  style= "float:right; background-color: #111;" type="button" onclick="logIn()" class="signupbtn">Log In</button>
 				<button style= "float:left; background-color: #111;" id = "signUpbtn" onclick="if (deadTimer == -1) pauseGame(); document.getElementById('signUp').style.display='block'; listening = false;" style="width:auto;">Sign Up</button>
@@ -36,7 +36,7 @@
 			<div id="login" class="modal">
 			  
 			  <form class="modal-content animate" action="/login.php">
-				<div class="imgcontainer">
+				<div class="imgcontainer"
 				  <span onclick="document.getElementById('login').style.display='none'; listening = true;" class="close" title="Close Modal">&times;</span>
 				</div>
 				<div class="container">
